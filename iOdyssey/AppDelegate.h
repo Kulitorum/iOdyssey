0cfb6e7865c75b648dba5b0ff255fcf0dc3e3cf9
kulitorum <apple@kulitorum.com>
2011-12-10 05:04:57 +0100

    Initial Commit

---
+++ iOdyssey/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOdyssey
+//
+//  Created by Michael Holm on 12/10/11.
+//  Copyright (c) 2011 Kulitorum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOdyssey/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iOdyssey
+//
+//  Created by Michael Holm on 12/10/11.
+//  Copyright (c) 2011 Kulitorum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
